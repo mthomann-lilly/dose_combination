1b3d0cc4f38b3afdf5fc7290801a0d951d361e20
Mitch Thomann <mitchell.thomann@lilly.com>
2018-04-11 11:57:28 -0400

    Plot Functions

--- work.R
+++ work.R
@@ -3,4 +3,17 @@ trt2=c(0,100,200,400)
 
 dat <- genDat_Combination() #get data
 
-analyze(dat_combination = dat,modText = combination)
+res <- analyze(dat_combination = dat,modText = combination)$long
+
+#test scatter3d
+par(mfrow=c(1,1))
+plot <- scatterplot3d(res,color="red",pch=16,grid=FALSE,box=FALSE)
+addgrids3d(res,grid=c("xy","xz","yz"),col.grid="black")
+
+#plotly
+
+p <- plot_ly(res,x=~trt1,y=~trt2,z=~mean,
+             marker=list(color=~mean,showscale=TRUE))
+p
+
+htmlwidgets::saveWidget(p,"/lrlhps/users/c216499/Projects/Documents/plot3d.html")
