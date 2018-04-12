aa96b6d06a68ef68a34f7094a97b0ebae4892316
Mitch Thomann <mitchell.thomann@lilly.com>
2018-04-12 17:41:51 -0400

    update plot

--- work.R
+++ work.R
@@ -4,6 +4,10 @@ trt2=c(0,100,200,400)
 dat <- genDat_Combination() #get data
 
 res <- analyze(dat_combination = dat,modText = combination)$long
+resMat <- analyze(dat_combination = dat,modText = combination)$mat
+resList <- list(x=matrix(as.numeric(rownames(resMat)),ncol=1),
+                y=matrix(as.numeric(colnames(resMat)),ncol=4),
+                z=resMat,type="surface")
 
 #test scatter3d
 par(mfrow=c(1,1))
@@ -11,9 +15,15 @@ plot <- scatterplot3d(res,color="red",pch=16,grid=FALSE,box=FALSE)
 addgrids3d(res,grid=c("xy","xz","yz"),col.grid="black")
 
 #plotly
-
+resMat <- as.matrix(res,ncol=3,byrow=F)
 p <- plot_ly(res,x=~trt1,y=~trt2,z=~mean,
-             marker=list(color=~mean,showscale=TRUE))
+             marker=list(color=~mean,showscale=TRUE,
+                         colorscale = list(c(0, "rgb(255, 0, 0)"), list(1, "rgb(0, 255, 0)"))))
 p
+
+#p <- plotly(resList),
+#             marker=list(color=~mean,showscale=TRUE,
+#                         colorscale = list(c(0, "rgb(255, 0, 0)"), list(1, "rgb(0, 255, 0)")))) %>% add_surface()
+#p
 
 htmlwidgets::saveWidget(p,"/lrlhps/users/c216499/Projects/Documents/plot3d.html")
