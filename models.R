f71c495f0278943e831c1be96e41d708e96ab875
Mitch Thomann <mitchell.thomann@lilly.com>
2018-04-30 17:12:03 -0400

    Linear Model

--- models.R
+++ models.R
@@ -1,4 +1,4 @@
-combination <- "
+combination_emax <- "
 model{
 for(i in 1:N){
 resp[i] ~ dbin(p[i],n[i])
@@ -17,6 +17,25 @@ ed50_12 ~ dnorm(400,0.00001)I(0,)
 for(i in 1:N){
 prob[i] <- ilogit(theta0 + emax1*dose1[i]/(ed50_1+dose1[i]) + emax2*dose2[i]/(ed50_2+dose2[i]) +
                emax12*dose12[i]/(ed50_12+dose12[i]))
+}
+
+}
+" 
+
+combination_linear <- "
+model{
+for(i in 1:N){
+resp[i] ~ dbin(p[i],n[i])
+logit(p[i]) <- theta0 + theta1*dose1[i] + theta2*dose2[i] + theta12*dose12[i]
+}
+
+theta0 ~ dnorm(0,0.25)
+theta1 ~ dnorm(0,0.25)
+theta2 ~ dnorm(0,0.25)
+theta12 ~ dnorm(0,0.25)
+
+for(i in 1:N){
+prob[i] <- ilogit(theta0 + theta1*dose1[i] + theta2*dose2[i] + theta12*dose12[i])
 }
 
 }
--- work.R
+++ work.R
@@ -3,8 +3,8 @@ trt2=c(0,100,200,400)
 
 dat <- genDat_Combination() #get data
 
-res <- analyze(dat_combination = dat,modText = combination)$long
-resMat <- analyze(dat_combination = dat,modText = combination)$mat
+#res <- analyze(dat_combination = dat,modText = combination_emax)$long
+resMat <- analyze(dat_combination = dat,modText = combination_emax)$mat
 resList <- list(x=matrix(as.numeric(rownames(resMat)),ncol=1),
                 y=matrix(as.numeric(colnames(resMat)),ncol=4),
                 z=resMat,type="surface")
@@ -17,3 +17,18 @@ p <- plot_ly(res,x=~trt1,y=~trt2,z=~mean,
 p
 
 htmlwidgets::saveWidget(p,"/lrlhps/users/c216499/Projects/Documents/plot3d.html")
+
+
+### linear
+#res <- analyze(dat_combination = dat,modText = combination_linear)$long
+resMat_lin <- analyze(dat_combination = dat,modText = combination_linear,plot_ed50 = FALSE)$mat
+resList <- list(x=matrix(as.numeric(rownames(resMat_lin)),ncol=1),
+                y=matrix(as.numeric(colnames(resMat_lin)),ncol=4),
+                z=resMat_lin,type="surface")
+
+#plotly
+resMat <- as.matrix(res,ncol=3,byrow=F)
+p2 <- plot_ly(res,x=~trt1,y=~trt2,z=~mean,
+             marker=list(color=~mean,showscale=TRUE,
+                         colorscale = list(c(0, "rgb(255, 0, 0)"), list(1, "rgb(0, 255, 0)"))))
+p2
